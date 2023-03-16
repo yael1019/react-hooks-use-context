1a403155fc813a2ff4299bf17b35ae00fc607546
Yael Harosh <yaelharosh01@gmail.com>
2023-03-15 20:41:10 -0400

    changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import ThemedButton from "./ThemedButton";
 import DarkModeToggle from "./DarkModeToggle";
 import defaultUser from "../data";
+import { UserContext } from '../context/user';
 
-function Header({ theme, setTheme, user, setUser }) {
+function Header({ theme, setTheme }) {
+
+  const {user, setUser} = useContext(UserContext);
+
   function handleLogin() {
     if (user) {
       setUser(null);
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
 import Interests from "./Interests";
+import { UserContext } from '../context/user';
 
-function Profile({ user, theme }) {
+function Profile({ theme }) {
+
+  const {user} = useContext(UserContext);
+
   if (!user) return <h2>Please Login To View Profile</h2>;
   return (
     <div>
